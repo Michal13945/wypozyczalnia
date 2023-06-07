3315a7882bcacdd684709e5d92cf459ba478aae3
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-06-07 22:45:16 +0200

    Add car list selection

--- wypozyczalnia/wypozyczalnia/MainWindow.xaml.cs
+++ wypozyczalnia/wypozyczalnia/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wypozyczalnia.Models;
 
-namespace wypozyczalnia
+namespace wypozyczalnia;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public ObservableCollection<Car> Cars { get; set; }
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        Cars = new ObservableCollection<Car>() {
+            new Car() {Id = 1, Model = "207", Brand = "Peugeot", Year = 2015},
+            new Car() {Id = 1, Model = "206", Brand = "Peugeot", Year = 2018},
+            new Car() {Id = 1, Model = "Octavia", Brand = "Skoda", Year = 2012},
+        };
+
+        InitializeComponent();
+
+        this.DataContext = this;
+    }
+
+    private void CarsSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+    }
+   
+    private void NextButton_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
