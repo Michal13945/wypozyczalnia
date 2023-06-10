b1a9bb6b4ef9efcc7f45150687aef92a2048c89f
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-06-10 22:33:09 +0200

    Add data base operations

---
+++ wypozyczalnia/wypozyczalnia/Data/DbData.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalnia.Models;
+
+namespace wypozyczalnia.Data;
+
+public static class DbData
+{
+    public static void AddCar(Car car)
+    {
+        using (var db = new AppDbContext())
+        {
+            db.Cars.Add(car);
+            db.SaveChanges();
+        }
+    }
+
+    public static Car? GetCar(int id)
+    {
+        using (var db = new AppDbContext())
+        {
+            return db.Cars.FirstOrDefault(x => x.Id == id);
+        }
+    }
+
+    public static IEnumerable<Car> GetCars()
+    {
+        using (var db = new AppDbContext())
+        {
+            return db.Cars
+                .Include(x => x.CarModel)
+                .ThenInclude(x => x.Brand)
+                .Include(x => x.RentedCars)
+                .ToList();
+        }
+    }
+
+    public static User? GetUser(string pesel)
+    {
+        using (var db = new AppDbContext())
+        {
+            return db.Users.FirstOrDefault(x => x.Pesel == pesel);
+        }
+    }
+
+    public static void AddUser(User user)
+    {
+        using (var db = new AppDbContext())
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+    }
+
+    public static void UpdateUser(User user)
+    {
+        using (var db = new AppDbContext())
+        {
+            db.Users.Update(user);
+            db.SaveChanges();
+        }
+    }
+
+    public static IEnumerable<User> GetUsers()
+    {
+        using (var db = new AppDbContext())
+        {
+            return db.Users.ToList();
+        }
+    }
+
+    public static void AddRentedCar(RentedCar rentedCar)
+    {
+        using (var db = new AppDbContext())
+        {
+            db.RentedCars.Add(rentedCar);
+            db.SaveChanges();
+        }
+    }
+
+    public static IEnumerable<RentedCar> GetRentedCars()
+    {
+        using (var db = new AppDbContext())
+        {
+            return db.RentedCars.ToList();
+        }
+    }
+}
