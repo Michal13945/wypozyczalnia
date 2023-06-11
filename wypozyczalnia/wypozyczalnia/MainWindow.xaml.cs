878e66441236c9af178dffc32ca24b95396f74c9
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-06-11 22:07:46 +0200

    Add rent cars in database

--- wypozyczalnia/wypozyczalnia/MainWindow.xaml.cs
+++ wypozyczalnia/wypozyczalnia/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wypozyczalnia.Data;
 using wypozyczalnia.Models;
 using wypozyczalnia.Windows;
 
@@ -21,30 +23,59 @@ namespace wypozyczalnia;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
     public ObservableCollection<Car> Cars { get; set; }
+    public Car? SelectedCar { get; set; }
+
+    public bool IsNextButtonEnabled { get; set; } = false;
+    public decimal PricePerDay { get; set; } = 0;
+    public string PriceVisibility { get; set; } = "Hidden";
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     public MainWindow()
     {
-        Cars = new ObservableCollection<Car>() {
-           
-        };
+        var cars = DbData.GetCars();
+
+        cars = cars.Where(x => x.RentedCars.Any(r => r.StartDate <= DateTime.Now && r.EndDate >= DateTime.Now) == false);
+
+        Cars = new ObservableCollection<Car>(cars);
 
         InitializeComponent();
 
         this.DataContext = this;
     }
 
-    private void CarsSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    protected virtual void OnPropertyChanged(string propertyName)
     {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-   
+
     private void NextButton_Click(object sender, RoutedEventArgs e)
     {
-        ClientInfoWindow clientInfoWindow = new ClientInfoWindow();
+        ClientInfoWindow clientInfoWindow = new ClientInfoWindow(SelectedCar.Id);
+
         clientInfoWindow.Show();
 
         this.Hide();
+    }
+
+    private void CarsSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var comboBox = sender as ComboBox;
+
+        var car = comboBox?.SelectedValue as Car;
+
+        SelectedCar = car;
+        PricePerDay = car.ValuePerDay;
+        PriceVisibility = "Visible";
+
+        IsNextButtonEnabled = true;
+
+        OnPropertyChanged("PricePerDay");
+        OnPropertyChanged("PriceVisibility");
+        OnPropertyChanged("IsNextButtonEnabled");
     }
 }
--- wypozyczalnia/wypozyczalnia/Windows/ClientInfoWindow.xaml.cs
+++ wypozyczalnia/wypozyczalnia/Windows/ClientInfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using wypozyczalnia.Data;
 using wypozyczalnia.Models;
 
 namespace wypozyczalnia.Windows
@@ -18,75 +20,165 @@ namespace wypozyczalnia.Windows
     /// <summary>
     /// Interaction logic for ClientInfoWindow.xaml
     /// </summary>
-    public partial class ClientInfoWindow : Window
+    public partial class ClientInfoWindow : Window, INotifyPropertyChanged
     {
         public User User { get; set; } = new User();
+        public RentedCar RentedCar { get; set; }
+        public Car? SelectedCar { get; set; }
 
-        public ClientInfoWindow()
+        public bool IsRentButtonEnabled { get; set; } = false;
+        public decimal Price { get; set; } = 0;
+
+        public ClientInfoWindow(int selectedCarId)
         {
+            SelectedCar = DbData.GetCar(selectedCarId);
+            RentedCar = new RentedCar() { CarId = SelectedCar.Id };
+
             InitializeComponent();
-        }
-        private void rentButton_Click(object sender, RoutedEventArgs e)
-        {
-            ConfirmationWindow confirmationWindow = new ConfirmationWindow();
 
-            confirmationWindow.Show();
-
-            this.Hide();
+            this.DataContext = this;
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         private void firstnameBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.Firstname = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void lastnameBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.Lastname = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void phoneNumberBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.PhoneNumber = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void peselBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.Pesel = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void streetBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.Street = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void cityBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.City = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void countryBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = sender as TextBox;
             User.Country = textBox?.Text;
+
+            ValidateButton();
         }
 
         private void startDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             var datePicker = sender as DatePicker;
             var startDate = datePicker?.SelectedDate;
+
+            if (startDate.HasValue)
+                RentedCar.StartDate = startDate.Value;
+
+            if (RentedCar.StartDate.HasValue && RentedCar.EndDate.HasValue)
+            {
+                var days = (RentedCar.EndDate - RentedCar.StartDate).Value.TotalDays + 1;
+                Price = Convert.ToDecimal(days) * SelectedCar.ValuePerDay;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+            }
+
+            ValidateButton();
         }
 
         private void endDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             var datePicker = sender as DatePicker;
             var endDate = datePicker?.SelectedDate;
+
+            if (endDate.HasValue)
+                RentedCar.EndDate = endDate.Value;
+
+            if (RentedCar.StartDate.HasValue && RentedCar.EndDate.HasValue)
+            {
+                var days = (RentedCar.EndDate - RentedCar.StartDate).Value.TotalDays + 1;
+                Price = Convert.ToDecimal(days) * SelectedCar.ValuePerDay;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+            }
+
+            ValidateButton();
         }
+
+
+        private bool IsFormValid()
+        {
+            return !string.IsNullOrEmpty(User.Firstname) &&
+                !string.IsNullOrEmpty(User.Lastname) &&
+                !string.IsNullOrEmpty(User.PhoneNumber) &&
+                !string.IsNullOrEmpty(User.Street) &&
+                !string.IsNullOrEmpty(User.City) &&
+                !string.IsNullOrEmpty(User.Country);
+        }
+
+        private void ValidateButton()
+        {
+            var formValid = IsFormValid();
+
+            if (formValid != IsRentButtonEnabled)
+            {
+                IsRentButtonEnabled = formValid;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRentButtonEnabled"));
+            }
+        }
+
+        private void rentButton_Click(object sender, RoutedEventArgs e)
+        {
+            var user = DbData.GetUser(User.Pesel);
+
+            if (user is null)
+            {
+                DbData.AddUser(User);
+                user = User;
+            }
+            else
+            {
+                DbData.UpdateUser(user);
+            }
+
+            RentedCar.UserId = user.Id;
+
+            DbData.AddRentedCar(RentedCar);
+
+            ConfirmationWindow confirmationWindow = new ConfirmationWindow();
+
+            confirmationWindow.Show();
+
+            this.Hide();
+        }
+    
     }
 }
